491b5dce007192d89433a1baca288a02f567ac23
50% <47536914+poligram@users.noreply.github.com>
2021-01-26 15:48:38 +0000

    Add files via upload

--- src/main/java/antidimension/antidimension/AntiDimension.java
+++ src/main/java/antidimension/antidimension/AntiDimension.java
@@ -1,27 +1,26 @@
 package antidimension.antidimension;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.PortalCreateEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AntiDimension extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         getServer().getPluginManager().registerEvents(new Listeners(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 
     public class Listeners implements Listener {
-        @EventHandler(priority = EventPriority.HIGH)
-        public void onPortalCreate(PortalCreateEvent e) {
+        @EventHandler(priority = EventPriority.HIGHEST)
+        public void onEnterPortal(PlayerPortalEvent e) {
             e.setCancelled(true);
         }
     }
